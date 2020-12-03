4d4a3c3b4662c22541824a6e64e1177cafc6ff4f
batoolmaali <71584273+batoolmaali@users.noreply.github.com>
2020-12-03 10:58:27 +0300

    login logout v3.1

---
+++ logout.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+
+unset($_SESSION['customer']);
+header('Location: http://localhost/E-Commerce Website PHP and MYSQL\CODE\ECOMMERCE-PUBLIC\goggles-web_Free07-08-2018_1255464790\web/index.php');
+
+ 
+?>
